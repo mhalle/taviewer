0903e9d5ef4a82b573519c685bbf454fcdd69920
Michael Halle <m -@- halle.us>
2018-01-14 01:25:41 -0500

    move wpImageIndex and nodeIndex move indexes from App state

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NodeIndex from './NodeIndex';
 import registerServiceWorker from './registerServiceWorker';
+import ta98Data from './human.json';
+import wpImageIndex from './wikipedia-images.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(<App 
+                   ta98Data={new NodeIndex(ta98Data)}
+                   wpImageIndex={wpImageIndex}/>, 
+                document.getElementById('root'));
 registerServiceWorker();
