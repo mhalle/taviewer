13e2b3ae6c012e29bef9e434ec940d5a367af396
Michael Halle <m -@- halle.us>
2018-06-01 12:25:12 -0400

    comment

--- src/NodeIndex.js
+++ src/NodeIndex.js
@@ -3,6 +3,9 @@ import _ from 'lodash';
 function index_tree(node_list, index) {
     index = index || {};
     _.forEach(node_list, node => {
+        // the node is represented by a tuple, 
+        // where node[0] is the ID 
+        // and node[6] is list of children of the node
         index[node[0]] = node;
         index_tree(node[6], index);
     });
