d250a059d92f5314863ce4baa0e514c7ca136043
Michael Halle <m -@- halle.us>
2018-06-01 12:27:30 -0400

    wikipedia-images.json not needed any more

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import App from './App';
 import NodeIndex from './NodeIndex';
 import registerServiceWorker from './registerServiceWorker';
 import ta98Data from './human.json';
-import wpImageIndex from './wikipedia-images.json';
 
 
 
 ReactDOM.render(<App 
                    ta98Data={new NodeIndex(ta98Data)}
-                   wpImageIndex={wpImageIndex}/>, 
+                />, 
                 document.getElementById('root'));
 registerServiceWorker();
