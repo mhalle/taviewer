fdd968100d2ebfa453b0644a5b5cf1cde5049501
Michael Halle <m -@- halle.us>
2018-07-03 21:31:59 -0400

    rename NodeIndex to node-index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import i18n from './i18n';
+
 import App from './App';
-import NodeIndex from './NodeIndex';
+import NodeIndex from './node-index';
 import registerServiceWorker from './registerServiceWorker';
 import ta98Data from './human.json';
-
 
 
 ReactDOM.render(<App 
